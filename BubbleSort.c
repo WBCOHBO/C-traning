7e164ae694d526f540ff3387ae1b102e67702320
WBCOHBO <andy405641@gmail.com>
2022-02-17 12:36:36 +0800

    Add string sorting

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+
+struct Student
+{
+    char name[21];
+    char Score;
+}Bubble[100],Exchange;
+
+
 int main(){
-    int HowManyNumbers,Bubble[100],Change,Count,Number;
+    int HowManyNumbers,Count,Number;
     
-    printf("How many number want to enter : ");
+    printf("How many student score do you want to enter : ");
     scanf("%d",&HowManyNumbers);
-    printf("Please type the numbers : ");
+    printf("Please enter the student name and score : \n");
     for(Count=1;Count<=HowManyNumbers;Count++){
-        scanf("%d",&Bubble[Count]);
+        scanf("%s %d",&Bubble[Count].name,&Bubble[Count].Score);
     }
     for(Count=1;Count<=HowManyNumbers-1;Count++){
         for(Number=1;Number<=HowManyNumbers-1;Number++){
-            if(Bubble[Number]<Bubble[Number+1]){
-                Change=Bubble[Number];
+            if(Bubble[Number].Score < Bubble[Number+1].Score){
+                Exchange=Bubble[Number];
                 Bubble[Number]=Bubble[Number+1];
-                Bubble[Number+1]=Change;
+                Bubble[Number+1]=Exchange;
             }
         }
     }
-    printf("Sort result from high to low : ");
+    printf("Sort result from high to low : \n");
     for(Count=1;Count<=HowManyNumbers;Count++){
-        printf("%d ",Bubble[Count]);
+        printf("%s %d\n",Bubble[Count].name,Bubble[Count].Score);
     }
 
     return 0;
