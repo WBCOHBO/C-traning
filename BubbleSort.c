1f1e9185506d84709044585095797b3679f6ccce
WBCOHBO <andy405641@gmail.com>
2022-03-10 11:38:09 +0800

    Add BubbleSort comment

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-struct Student
+struct Student  //定義結構
 {
     char name[21];
     char Score;
-}Bubble[100],Exchange;
+}Bubble[100],Exchange;  //Bubble[] and Exchange為Student的代理變數
 
 
 int main(){
     int HowManyNumbers,Count,Number;
     
     printf("How many student score do you want to enter : ");
-    scanf("%d",&HowManyNumbers);
+    scanf("%d",&HowManyNumbers);    //輸入n個數
     printf("Please enter the student name and score : \n");
     for(Count=1;Count<=HowManyNumbers;Count++){
-        scanf("%s %d",&Bubble[Count].name,&Bubble[Count].Score);
+        scanf("%s %d",&Bubble[Count].name,&Bubble[Count].Score); //迴圈讀入n個人名與分數
     }
     for(Count=1;Count<=HowManyNumbers-1;Count++){
         for(Number=1;Number<=HowManyNumbers-1;Number++){
-            if(Bubble[Number].Score < Bubble[Number+1].Score){
+            if(Bubble[Number].Score < Bubble[Number+1].Score){  //從陣列第1個開始依序與下一個做比較，若由大到小排序則須把小排右大排左
+                                                                //Bubble[1]與Bubble[2]比，Bubble[2]與Bubble[3]比... ...Bubble[9]與Bubble[10]比，總共比n-1次。    
                 Exchange=Bubble[Number];
                 Bubble[Number]=Bubble[Number+1];
                 Bubble[Number+1]=Exchange;
